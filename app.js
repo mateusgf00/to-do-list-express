7f3d4c712c7cacc1fc0c7ebe3aa01a734a8ddba6
Mateus Ferreira <mateusgf00@gmail.com>
2022-03-23 23:33:07 -0300

    utilising package methodOverride

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const path = require('path');
 
 const checklistRouter = require('./src/routes/checklist');
 const rootRouter = require('./src/routes/index');
+const methodOverride = require('method-override');
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
