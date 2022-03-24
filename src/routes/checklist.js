abc482a875f722f769f6233ffc1c9a291a7e3cc3
Mateus Ferreira <mateusgf00@gmail.com>
2022-03-23 23:28:18 -0300

    create method POST

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -4,12 +4,43 @@ const router = express.Router();
 
 const Checklist = require('../models/checklists');
 
-router.delete('/:id', async(req,res) =>{
+router.get('/', async(req, res) =>{
+    
     try{
-        let checklist = await Checklist.findByIdAndDelete(req.params.id);
-        res.status(200).json(checklists);
+        let checklist = await Checklist.find({});
+        res.status(200).render('checklists/index', {checklist:checklist})
     }catch(error){
-        res.status(422).json(error);
+        res.status(200).render('pages/error', {error:'Erro ao exibir a listas'})
+    }
+})
+
+    router.get('/new', async(req, res) =>{
+        try{
+            let checklist = new Checklist();
+            res.status(200).render('checklists/new', {checklist: checklist});
+        }catch(error){
+            res.status(500).render('pages/error', {errors: 'Erros ao carregar o formulario'});
+        }
+    })
+
+router.post('/', async(req, res) =>{
+    let {name} = req.body.checklist;
+    let checklist = new Checklist({name});
+
+    try{
+        await checklist.save();
+        res.redirect('/checklists');
+    }catch(error){
+        res.status(422).render('checklists/new', {checklists:{...Checklist, error}})
+    }
+})
+
+router.get('/:id',async(req, res) =>{
+    try{
+        let checklist = await Checklist.findById(req.params.id);
+        res.status(200).render('checklists/show', {checklist:checklist})
+    }catch(error){
+        res.status(500).render('pages/error', {error:'Erro ao exibir a listas'})
     }
 })
 
@@ -23,33 +54,12 @@ router.put('/:id',async(req, res) =>{
     }
 })
 
-router.get('/:id',async(req, res) =>{
+router.delete('/:id', async(req,res) =>{
     try{
-        let checklists = await Checklist.findById(req.params.id);
-        res.status(200).render('checklists/show', {checklists:checklists})
+        let checklist = await Checklist.findByIdAndDelete(req.params.id);
+        res.status(200).json(checklists);
     }catch(error){
-        res.status(200).render('pages/error', {error:'Erro ao exibir a listas'})
-    }
-})
-
-router.get('/', async(req, res) =>{
-    
-    try{
-        let checklist = await Checklist.find({});
-        res.status(200).render('checklists/index', {checklist:checklist})
-    }catch(error){
-        res.status(200).render('pages/error', {error:'Erro ao exibir a listas'})
-    }
-})
-
-router.post('/', async(req, res) =>{
-    let {name} = req.body;
-
-    try{
-        let checklist = await Checklist.create({name});
-        res.status(200).json(checklist);
-    }catch(error){
-        res.status(422).json(error)
+        res.status(422).json(error);
     }
 })
 
